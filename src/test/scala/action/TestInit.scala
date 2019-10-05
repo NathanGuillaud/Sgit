cdad805a06cd4659ac638c1cf87795d4e82b08cc
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-10-05 19:57:41 +0200

    Test init

---
+++ src/test/scala/action/TestInit.scala
@@ -0,0 +1,20 @@
+package action
+
+import java.io.File
+import org.scalatest.FunSuite
+import action.Creation
+
+class TestInit extends FunSuite {
+  test("sgit init should create .sgit directory with directories and files") {
+    Creation.init()
+    assert(new File(".sgit").exists())
+    assert(new File(".sgit/objects/tree").exists())
+    assert(new File(".sgit/objects/blob").exists())
+    assert(new File(".sgit/objects/commit").exists())
+    assert(new File(".sgit/HEAD").exists())
+    assert(new File(".sgit/refs/tags").exists())
+    assert(new File(".sgit/refs/tags").exists())
+    assert(new File(".sgit/config").exists())
+    assert(new File(".sgit/branches").exists())
+  }
+}
