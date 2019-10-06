4295db43a25814122c2474a554e44725d8e57f20
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-10-06 23:42:59 +0200

    Implémentation tag

--- src/main/scala/action/Tag.scala
+++ src/main/scala/action/Tag.scala
@@ -1,9 +1,29 @@
 package action
 
+import java.io.File
+import java.nio.file.{Files, Paths}
+
+import util.{FileManagement, SgitTools}
+
+import scala.io.Source
+
 case class Tag()
 
 object Tag {
   def tag(command: Array[String]): Unit = {
-
+    val currentBranch = SgitTools.getCurrentBranch()
+    if(Files.notExists(Paths.get(".sgit/refs/tags"))) {
+      println("You have to make a first commit before create a new tag")
+    }
+    //If the tag already exists
+    else if(Files.exists(Paths.get(".sgit/refs/tags/" + command(0)))){
+      println("The tag " + command(0) + " already exists")
+    } else {
+      //Retrieve current commit
+      val currentCommit = Source.fromFile(".sgit/refs/heads/" + currentBranch).getLines.mkString("\n")
+      //Write head into refs
+      new File(".sgit/refs/tags/" + command(0)).createNewFile()
+      FileManagement.writeFile(".sgit/refs/tags/" + command(0), currentCommit)
+    }
   }
 }
---
+++ src/test/scala/action/TestTag.scala
@@ -0,0 +1,16 @@
+package action
+
+import java.io.File
+
+import org.scalatest.FunSuite
+
+import scala.io.Source
+
+class TestTag extends FunSuite {
+  /*test("sgit tag should create a reference of the tag in .sgit directory") {
+    Init.init()
+    Tag.tag(Array("q4s5d6"))
+    assert(new File(".sgit/refs/tags/q4s5d6").exists())
+    assert(Source.fromFile(".sgit/refs/tags/q4s5d6").getLines.length == 1)
+  }*/
+}
