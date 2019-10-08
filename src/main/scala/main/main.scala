05fc35899f6ae2fe9204f5931011cadcbcead254
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-10-09 00:03:28 +0200

    Traduction en anglais messages print

--- src/main/scala/main/main.scala
+++ src/main/scala/main/main.scala
@@ -3,7 +3,7 @@ import action._
 
 object Main extends App{
   if (args.length == 0) {
-    println("Vous devez mentionner une action")
+    println("You must specify an action")
   } else {
     val command = args
     eval(command)
@@ -21,73 +21,73 @@ object Main extends App{
     case "tag" => tag(command)
     case "merge" => merge(command)
     case "rebase" => rebase(command)
-    case default => println("Cette action n'existe pas")
+    case default => println("This action does not exist")
   }
 
   def init(command: Array[String]): Unit = command match {
     case Array("init") => Init.init()
-    case default => println("La commande sgit init ne contient pas d'option")
+    case default => println("Command sgit init has no option")
   }
 
   def status(command: Array[String]): Unit = command match {
     case Array("status") => Status.status()
-    case default => println("La commande sgit status ne contient pas d'option")
+    case default => println("Command sgit status has no option")
   }
 
   def diff(command: Array[String]): Unit = command match {
     case Array("diff") => Diff.diff()
-    case default => println("La commande sgit diff ne contient pas d'option")
+    case default => println("Command sgit diff has no option")
   }
 
   def add(command: Array[String]): Unit = command match {
-    case Array("add") => println("Vous devez préciser les fichiers à ajouter avec la commande sgit add <files>")
+    case Array("add") => println("You must specify files to add with the command sgit add <files>")
     case default => Add.add(command.tail)
   }
 
   def commit(command: Array[String]): Unit = command match {
     case Array("commit") => CommitAction.commit()
-    case default => println("La commande sgit commit ne contient pas d'option")
+    case default => println("Command sgit commit has no option")
   }
 
   def log(command: Array[String]): Unit = command match {
     case Array("log") => Log.log()
     case Array("log", "-p") => Log.logP()
     case Array("log", "--stat") => Log.logStat()
-    case Array("log", "-p", _*) => println("La commande sgit log -p ne contient pas d'autres options")
-    case Array("log", "--stat", _*) => println("La commande sgit log --stat ne contient pas d'autres options")
-    case default => println("La commande log ne contient pas cette option, essayez sgit log -p ou sgit log --stat")
+    case Array("log", "-p", _*) => println("Command sgit log -p does not contain any other option")
+    case Array("log", "--stat", _*) => println("Command sgit log --stat does not contain any other option")
+    case default => println("Command log has no option, try sgit log -p or sgit log --stat")
   }
 
   def branch(command: Array[String]): Unit = command match {
-    case Array("branch") => println("Vous devez préciser le nom de la branche à créer")
+    case Array("branch") => println("You must specify the branch name")
     case Array("branch", "-av") => Branch.branchAV()
     case Array("branch", _) => Branch.branch(command.tail)
-    case Array("branch", "-av", _*) => println("La commande sgit branch -av ne contient pas d'autres options")
-    case default => println("Le nom de la branche à créer ne doit pas contenir d'espace. Essayez sgit branch <branch> ou sgit branch -av")
+    case Array("branch", "-av", _*) => println("Command sgit branch -av does not contain any other option")
+    case default => println("The branch name must not contain spaces. Try sgit branch <branch> or sgit branch -av")
   }
 
   def checkout(command: Array[String]): Unit = command match {
-    case Array("checkout") => println("Vous devez préciser le nom de la branche, du tag ou le numéro du commit sur lequel se placer")
+    case Array("checkout") => println("You must specify the name of the branch, the tag or the commit hash on which to place")
     case Array("checkout", _) => Checkout.checkout(command.tail)
-    case default => println("La branche, le tag ou le numéro du commit ne doivent pas contenir d'espace")
+    case default => println("The branch, the tag or the commit hash must not contain spaces")
   }
 
   def tag(command: Array[String]): Unit = command match {
-    case Array("tag") => println("Vous devez préciser le nom du tag")
+    case Array("tag") => println("You must specify the tag name")
     case Array("tag", _) => Tag.tag(command.tail)
-    case default => println("Le nom du tag ne doit pas contenir d'espace")
+    case default => println("The tag name must not contain spaces")
   }
 
   def merge(command: Array[String]): Unit = command match {
-    case Array("merge") => println("Vous devez préciser le nom de la branche pour le merge")
+    case Array("merge") => println("You must specify the branch name for the merge command")
     case Array("merge", _) => Merge.merge(command.tail)
-    case default => println("Le nom de la branche pour le merge ne doit pas contenir d'espace")
+    case default => println("The branch name must not contain spaces")
   }
 
   def rebase(command: Array[String]): Unit = command match {
-    case Array("rebase") | Array("rebase", "-i") => println("Vous devez préciser le nom de la branche pour le rebase")
+    case Array("rebase") | Array("rebase", "-i") => println("You must specify the branch name for the rebase command")
     case Array("rebase", "-i", _) => Rebase.rebaseI(command.tail)
     case Array("rebase", _) => Rebase.rebase(command.tail)
-    case default => println("Le nom de la branche à créer ne doit pas contenir d'espace. Essayez sgit rebase <branch> ou sgit rebase -i <branch or commit>")
+    case default => println("The branch name must not contain spaces. Try sgit rebase <branch> or sgit rebase -i <branch or commit>")
   }
 }
