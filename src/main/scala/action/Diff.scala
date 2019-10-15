e1b7c0513e4231594016f8913956c096264fb8f2
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-10-15 14:52:58 +0200

    Implémentation commande sgit diff

--- src/main/scala/action/Diff.scala
+++ src/main/scala/action/Diff.scala
@@ -1,7 +1,10 @@
 package action
 
+import java.io.File
+import java.nio.file.{Files, Paths}
+
 import model.Delta
-import util.PathManagement
+import util.{FileManagement, PathManagement, SgitTools, StageManagement}
 
 object Diff {
 
@@ -9,24 +12,54 @@ object Diff {
     if(PathManagement.getSgitPath().isEmpty){
       println("fatal: Not a sgit repository (or any of the parent directories): .sgit")
     } else {
-      println("DIFF")
-      val deltas = getDeltasBetweenString(List("e","b","d"), List("a","b","d","e"))
-      println("----------")
-      println("Voici les deltas :")
-      deltas.map(x => println(x.line + " " + x.action + " " + x.content))
+      val currentBranch = SgitTools.getCurrentBranch()
+      val files = StageManagement.getStageContent(currentBranch)
+      //Print deltas for each file in the stage
+      files.map(file =>
+        if(Files.exists(Paths.get(PathManagement.getSgitPath().get + "/objects/blob/" + file(1).substring(0,2) + "/" + file(1).substring(2)))) {
+          //If the old and new files exists
+          if(Files.exists(Paths.get(PathManagement.getProjectPath().get + "/" + file(0)))) {
+            val newFileContent = FileManagement.readFile(new File(PathManagement.getProjectPath().get + "/" + file(0)))
+            val newFileHash = FileManagement.hashFile(file(0).split("/").last, newFileContent)
+            printDiff(file(0), file(1), newFileHash, getDeltasBetweenFiles(FileManagement.readFile(new File(PathManagement.getSgitPath().get + "/objects/blob/" + file(1).substring(0,2) + "/" + file(1).substring(2))).split("\n").toList, newFileContent.split("\n").toList))
+          }
+          //If the new file was removed
+          else {
+            printDiff(file(0), file(1), "0000000", getDeltasBetweenFiles(FileManagement.readFile(new File(PathManagement.getSgitPath().get + "/objects/blob/" + file(1).substring(0,2) + "/" + file(1).substring(2))).split("\n").toList, List[String]()))
+          }
+        }
+      )
     }
   }
 
-  def getDeltasBetweenString(oldList: List[String], newList: List[String]): List[Delta] = {
-    //Create an empty matrix
-    var matrix = initializeMatrix(oldList, newList)
-    println("Matrice avec 0 :")
-    println("Pour 0,0 : " + matrix(0)(0))
-    println("Pour le dernier elem : " + matrix(oldList.length)(newList.length))
-    //Fill the matrix with deltas between the 2 lists in parameters
-    matrix = fillMatrix(oldList, newList, 1, 1, matrix)
-    //Retrieve the deltas from the matrix
-    getDeltasFromMatrix(oldList, newList, oldList.length, newList.length, matrix, List[Delta]())
+  //Print the differences to the console
+  def printDiff(filePath: String, oldFileHash: String, newFileHash: String, deltas: List[Delta]): Unit = {
+    if(!deltas.isEmpty) {
+      println("diff --git a/" + filePath + " b/" + filePath)
+      if(newFileHash == "0000000") println("deleted file")
+      println("index " + oldFileHash.substring(0,7) + ".." + newFileHash.substring(0,7))
+      deltas.map(delta =>
+        if(delta.action == "+") println(Console.GREEN + delta.action + delta.content + Console.WHITE)
+        else println(Console.RED + delta.action + delta.content + Console.WHITE)
+      )
+      println()
+    }
+  }
+
+  def getDeltasBetweenFiles(oldList: List[String], newList: List[String]): List[Delta] = {
+    //If the newFile is empty
+    if(newList.isEmpty){
+      var deltasToReturn = List[Delta]()
+      oldList.map(line => deltasToReturn = new Delta(0, "-", line) :: deltasToReturn)
+      deltasToReturn
+    } else {
+      //Create an empty matrix
+      var matrix = initializeMatrix(oldList, newList)
+      //Fill the matrix with deltas between the 2 lists in parameters
+      matrix = fillMatrix(oldList, newList, 1, 1, matrix)
+      //Retrieve the deltas from the matrix
+      getDeltasFromMatrix(oldList, newList, oldList.length, newList.length, matrix, List[Delta]())
+    }
   }
 
   //Create an empty matrix with a size of oldList+1 X newList+1
@@ -42,7 +75,6 @@ object Diff {
       //If values of the old list and the new list are the same
       if(oldList(i-1) == newList(j-1)) matrix(i)(j) = matrix(i-1)(j-1)+1
       else matrix(i)(j) = Math.max(matrix(i)(j-1), matrix(i-1)(j))
-      println(i + "-" + j + " : " + matrix(i)(j))
       //If it is the end of the line
       if(j == newList.length) fillMatrix(oldList, newList, i+1, 1, matrix)
       else fillMatrix(oldList, newList, i, j+1, matrix)
@@ -54,7 +86,6 @@ object Diff {
     //If we go up the 2 lists
     if(i == 0 && j == 0) deltas
     else {
-      println(i + "-" + j)
       //If the new list contains a new element
       if(i == 0 || (j > 0 && matrix(i)(j-1) == matrix(i)(j))) getDeltasFromMatrix(oldList, newList, i, j-1, matrix, new Delta(j, "+", newList(j-1)) :: deltas)
       //If the old list contains an element removed
